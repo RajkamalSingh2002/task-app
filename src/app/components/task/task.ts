9afe26b17635ec61b2f012ee81330441ded3403f
RajkamalSingh2002 <rajdhindsa2002@gmail.com>
2026-03-19 11:25:46 -0400

    Update task.ts

--- src/app/components/task/task.ts
+++ src/app/components/task/task.ts
@@ -1,1 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+export interface TaskType {
+  id: number;
+  title: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.html',
+  styleUrls: ['./task.css']
+})
+export class TaskComponent {
+  @Input() task!: TaskType;
+  @Output() delete = new EventEmitter<number>();
+
+  onDelete() {
+    this.delete.emit(this.task.id);
+  }
+}
